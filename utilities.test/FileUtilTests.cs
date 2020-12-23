edb792b30ce1880d3c370956c19f35229411f1d7
marko <marko.meinander@iki.fi>
2020-12-23 15:12:21 +0200

    Added FileUtil.GetSafeFilename()

---
+++ utilities.test/FileUtilTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace utilities.test
+{
+   [TestClass]
+   public class FileUtilTests
+   {
+      [TestMethod]
+      public void GetSafeFilename_GoodFilename_NoChanges()
+      {
+         var goodFilename = "file.txt";
+         Assert.AreEqual(goodFilename, FileUtil.GetSafeFilename(goodFilename));
+      }
+
+      [TestMethod]
+      public void GetSafeFilename_BadFilename_BadCharactersReplacedWithUnderscore()
+      {
+         var badFilename = ">fi/l?e.t*t";
+         var expectedFilename = "_fi_l_e.t_t";
+         Assert.AreEqual(expectedFilename, FileUtil.GetSafeFilename(badFilename));
+      }
+
+      [TestMethod]
+      public void GetSafeFilename_InputNull_ReturnsNull()
+      {
+         Assert.IsNull(FileUtil.GetSafeFilename(null));
+      }
+
+      [TestMethod]
+      public void GetSafeFilename_InputWhiteSpace_ReturnsTextWHITESPACE()
+      {
+         Assert.AreEqual(FileUtil.WHITESPACE, FileUtil.GetSafeFilename("  \t   "));
+         Assert.AreEqual(FileUtil.WHITESPACE, FileUtil.GetSafeFilename(string.Empty));
+      }
+   }
+}
---
+++ utilities/FileUtil.cs
@@ -0,0 +1,33 @@
+﻿namespace utilities
+{
+   public static class FileUtil
+   {
+      public static string WHITESPACE => "WHITESPACE";
+
+      /// <summary>
+      /// Converts input to a safe filename. Bad characters are replaced with underscores '_'.
+      ///
+      /// For null returns null.
+      /// For whitespace returns "WHITESPACE"
+      /// </summary>
+      /// <param name="filename">Filename to convert</param>
+      /// <returns></returns>
+      public static string GetSafeFilename(string filename)
+      {
+         if (string.IsNullOrWhiteSpace(filename))
+         {
+            if (filename == null)
+            {
+               return null;
+            }
+            else
+            {
+               // filename that is only whitespace does not make sense
+               return WHITESPACE;
+            }
+         }
+         // Convert bad characters to '_'
+         return string.Join("_", filename.Split(System.IO.Path.GetInvalidFileNameChars()));
+      }
+   }
+}
